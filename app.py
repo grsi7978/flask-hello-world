ddb0cc95b83e99a2260148d903bdbb556b0aa269
Graydon Sinclair <grsi7978@colorado.edu>
2024-03-25 20:06:21 +0000

    Basketball Table Selecting

--- app.py
+++ app.py
@@ -45,3 +45,22 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgres://lab10_render_db_user:3nbt8zcuJiN8YOWYQLxxkaBbvYWQ6k3U@dpg-co0t727109ks73biivk0-a/lab10_render_db")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball; 
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
