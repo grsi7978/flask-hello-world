a86a740cd98db5c2d6f50a6e8f55cf2111a4fc24
Graydon Sinclair <grsi7978@colorado.edu>
2024-03-25 20:08:35 +0000

    Basketball Table Dropping

--- app.py
+++ app.py
@@ -64,3 +64,14 @@ def selecting():
         response_string+="</tr>"
     response_string+="</table>"
     return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgres://lab10_render_db_user:3nbt8zcuJiN8YOWYQLxxkaBbvYWQ6k3U@dpg-co0t727109ks73biivk0-a/lab10_render_db")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
