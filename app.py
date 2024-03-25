23872ca5a5f921dc1cddf9f82b697987df6533c1
Graydon Sinclair <grsi7978@colorado.edu>
2024-03-25 19:50:27 +0000

    Typo fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Graydon Sinclair in 3308'
 
-@app.rout('/db_test')
+@app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgres://lab10_render_db_user:3nbt8zcuJiN8YOWYQLxxkaBbvYWQ6k3U@dpg-co0t727109ks73biivk0-a/lab10_render_db")
     conn.close()
