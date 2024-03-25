168f94f4fd2d8149e4395c3414c05f9dd8bf96d6
Graydon Sinclair <grsi7978@colorado.edu>
2024-03-25 19:56:16 +0000

    Basketball Table Created

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn = psycopg2.connect("postgres://lab10_render_db_user:3nbt8zcuJiN8YOWYQLxxkaBbvYWQ6k3U@dpg-co0t727109ks73biivk0-a/lab10_render_db")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('db_create')
+def creating():
+    conn = psycopg2.connect("postgres://lab10_render_db_user:3nbt8zcuJiN8YOWYQLxxkaBbvYWQ6k3U@dpg-co0t727109ks73biivk0-a/lab10_render_db")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
